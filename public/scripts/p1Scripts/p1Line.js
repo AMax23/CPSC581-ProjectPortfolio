dc24ae1d28e11d75f930dc12ace138cc1cddcb21
Ahmed <ahmed.hasan@ucalgary.ca>
2021-02-07 16:30:35 -0700

    Add mic input to drawing

---
+++ public/scripts/p1Scripts/p1Line.js
@@ -0,0 +1,47 @@
+// JavaScript source code for the line constructor.
+
+function Line(posX, posY, prevX, prevY) {
+    this.width = posX;
+    this.height = posY;
+    this.x = posX;
+    this.y = posY;
+    this.prevX = prevX;
+    this.prevY = prevY;
+    // Assign a random RGB colour when instantiated.
+    this.colourR = random(255);
+    this.colourG = random(255);
+    this.colourB = random(255);
+
+    // Draw a line with the x and y coords.
+    this.show = function () {
+        stroke(this.colourR, this.colourG, this.colourB);
+        //stroke(255);
+        line(this.prevX, this.prevY, this.x, this.y);
+    };
+
+    // Move the x and y coords of the line.
+    this.move = function (soundLevel) {
+
+        // Prev x and y coords become current.
+        this.prevX = this.x;
+        this.prevY = this.y;
+
+        var val = floor(random(4)); // Generate random number between 0 and 3.
+
+        if (val == 0) {
+            this.x = this.x + soundLevel;
+        } else if (val == 1) {
+            this.y = this.y + soundLevel;
+        } else if (val == 2) {
+            this.x = this.x - soundLevel;
+        } else {
+            this.y = this.y - soundLevel;
+        }
+
+        // Make sure that the line stays inside the canvas.
+        this.x = constrain(this.x, 10, width - 10);
+        this.y = constrain(this.y, 10, height - 10);
+    }
+
+}
+
--- public/scripts/p1Scripts/p1Sketch.js
+++ public/scripts/p1Scripts/p1Sketch.js
@@ -2,16 +2,12 @@
 
 var mic;
 
+var userLine;
+
 // Canvas
 var canvasWidth = 600;
 var canvasHeight = 400;
 var canvasColour = 0; // Black
-
-// The line will start from the center of the canvas.
-var lineX = canvasWidth / 2;
-var lineY = canvasHeight / 2;
-var prevLineX = lineX;
-var prevLineY = lineY;
 
 ////////////////////////// BASIC P5 SET UP ////////////////////////////////////////
 function setup() {
@@ -22,6 +18,14 @@ function setup() {
     // Create and start an Audio input
     mic = new p5.AudioIn();
     mic.start();
+
+    // The line will start from the center of the canvas.
+    var lineX = canvasWidth / 2;
+    var lineY = canvasHeight / 2;
+    var prevLineX = lineX;
+    var prevLineY = lineY;
+    userLine = new Line(lineX, lineY, prevLineX, prevLineY);
+
 }
 
 function draw() {
@@ -30,21 +34,10 @@ function draw() {
 ///////////////////////////////////////////////////////////////////////////////////
 
 function drawLine() {
-    // Draw a line with the x and y coords
-    stroke(255);
-    line(prevLineX, prevLineY, lineX, lineY);
 
     var volumeLevel = mic.getLevel(); // Read the amplitude (volume level).
-    var soundLevel = volumeLevel * 10; // This volume level is between 0–1 which is too small.
-
-    console.log('sound Level = ' + soundLevel);
-    //console.log(lineX + ', ' + lineY);
-
-    // Prev x and y coords become current.
-    prevLineX = lineX;
-    prevLineY = lineY;
-
-    // Move line based on the sound level.
-    lineY = lineY + soundLevel;
-    lineX = lineX + soundLevel;
+    var soundLevel = volumeLevel * 100; // This volume level is between 0–1 which is too small.
+    //print(soundLevel);
+    userLine.show();
+    userLine.move(soundLevel);
 }
