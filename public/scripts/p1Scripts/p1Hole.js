a6706e4906e3d6a7b77f7165ca633bd95b36fc7f
Ahmed <ahmed.hasan@ucalgary.ca>
2021-02-12 08:29:03 -0700

    Cleanup and documentation

--- public/scripts/p1Scripts/p1Hole.js
+++ public/scripts/p1Scripts/p1Hole.js
@@ -4,13 +4,8 @@ function Hole(posX, posY, img) {
     this.x = posX;
     this.y = posY;
     this.img = img;
-    //this.extraCanvas = createGraphics(100, 200);
 
     this.show = function () {
-        //this.extraCanvas.clear();
-
-        //this.extraCanvas.image(this.img, 0, 0, this.img.width / 7, this.img.height / 7);
-        //image(this.extraCanvas, this.x, this.y);
         image(img, this.x, this.y, this.img.width / 7, this.img.height / 7);
     }
 }
--- public/scripts/p1Scripts/p1Mole.js
+++ public/scripts/p1Scripts/p1Mole.js
@@ -1,41 +1,40 @@
-// JavaScript source code
+// JavaScript source code. Function constructor for Moles.
 
 function Mole(posX, posY, img) {
 
     this.x = posX;
     this.y = posY;
     this.img = img;
-    this.speed = 1;
+    this.speed = 2;
     this.hit = false;
-    this.extraCanvas = createGraphics(100, 50);
-    this.bound = 50;
+    this.extraCanvas = createGraphics(100, 50); // This size is from trial and error to see what works best.
+    this.bound = 50; // The point at which the mole is fully out or all in.
     this.hideY = this.bound;
 
     this.show = function () {
-        //this.extraCanvas.background(0);
+        /* This is a buffer canvas drawn on top of the original one with a smaller size.
+         * The moles are drawn on this canvas instead of the main to show the effect on hiding
+         * Once the mole goes out of its canvas it is not shown which makes it look like it went in the hole.
+         */
         this.extraCanvas.image(this.img, 0, this.hideY, this.img.width / 7, this.img.height / 7);
-
-        image(this.extraCanvas, this.x, this.y);
-        //console.log('mole height = ' + img.height / 7);
+        image(this.extraCanvas, this.x, this.y); // Put this extra canvas exactly where the hole is so its aligned.
 
         if (this.hideY == 0) {
             this.hideY = 0;
         } else {
-            this.hideY = this.hideY - 1;
+            this.hideY = this.hideY - this.speed;
         }
-        //image(this.img, this.x, this.y, this.img.width / 7, this.img.height / 7);
     }
 
     this.hide = function () {
-        //this.extraCanvas.background(0);
+        // Same logic as showing the mole but now we are going down.
         this.extraCanvas.image(this.img, 0, this.hideY, this.img.width / 7, this.img.height / 7);
         image(this.extraCanvas, this.x, this.y);
-        //console.log('mole height = ' + img.height / 7);
 
         if (this.hideY >= this.bound) {
             this.y = this.bound;
         } else {
-            this.hideY = this.hideY + 1;
+            this.hideY = this.hideY + this.speed;
         }
     }
 
