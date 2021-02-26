9e8cd21425a27f63af3b45a70d8bdac8f13f592a
Ahmed <ahmed.hasan@ucalgary.ca>
2021-02-25 17:55:05 -0700

    Fix sound problem (testing)

--- public/scripts/p1Scripts/p1Mic.js
+++ public/scripts/p1Scripts/p1Mic.js
@@ -8,7 +8,7 @@ function Microphone() {
     this.constraints = { audio: true };
     this.whackSound = new Audio('../sounds/project 1/whackSound.wav');
     this.bombSound = new Audio('../sounds/project 1/bombSound.wav');
-    this.gameMusic = new Audio('../sounds/project 1/moonBaseSound.mp3');
+    this.gameMusic = new Audio('../sounds/project 1/moonBaseMusic.mp3');
     this.audioPermission = false;
 
     this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
@@ -37,11 +37,15 @@ function Microphone() {
                 document.getElementById('body').onclick = function () {
                     if (!self.audioPermission) {
                         self.audioPermission = true;
+                        // These sounds need to play the first time, but im just gonna mute them
+                        // so no one knows whats really happening! Stop me if you can.
+                        self.gameMusic.play();
+                        self.gameMusic.volume = 0;
 
                         // Play the bomb sound the first time user clicks.
                         // This is just so the bomb sound works in game.
-                        self.bombSound.volume = 0;
                         self.bombSound.play();
+                        self.bombSound.volume = 0;
                         self.bombSound.currentTime = 0;
 
                         self.whackSound.play();
