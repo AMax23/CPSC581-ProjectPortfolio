922f5c96e8d684f823910a95a23bf8346b34ea94
Ahmed <ahmed.hasan@ucalgary.ca>
2021-02-27 16:26:52 -0700

    Add sketches for project 1

--- public/scripts/p0Scripts/p0Details.js
+++ public/scripts/p0Scripts/p0Details.js
@@ -2,14 +2,18 @@
 // For animating the slide caption
 $(document).ready(function () {
 
+    let totalImages = 0;
+    // Get the total number of images.
+    $('.totalImagesNumber').each(function (i, obj) {
+        totalImages++;
+    });
+
     $(".carousel-caption h5").addClass('animated slideInLeft');
 
     $(".carousel-caption p").addClass('animated slideInRight');
 
     // Numbers all the 20 images
     $('.totalImagesNumber').each(function (i, obj) {
-        obj.innerHTML  = i+1 + ' ' + ' / 20';
+        obj.innerHTML = i + 1 + ' ' + ' / ' + totalImages;
     });
 });
-
-
